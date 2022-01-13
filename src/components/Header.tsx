f42994d299ff81066a2f705d04117e6c3f74e960
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-01-13 14:02:05 -0300

    Header: layout finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles.css'
 import LogoHeader from '../assets/images/logo.svg'
 import SearchBar from './SearchBar/SearchBar'
 
 const Header = (props: any) => {
   return (
-    <div className='container'>
-      <div className='container-header'>
+    <div className='container-header'>
+      <div className='box-header'>
         <div className='container-logo-header'>
           <img src={LogoHeader} alt="Logo do site" />
         </div>
         <SearchBar
+          className='search-movie'
           placeholder='Pesquisar filme...'
         />
         <div className='container-menu-header'>
           <ul className='menu-header'>
-            <li>
+            <li className='menu-dropdown'>
               <a href="#">programação</a>
-              <div className='sub-menu-header'>
-                <a href="#"></a>
-              </div>
+              <ul className='sub-menu-header'>
+                <li>
+                  <a href="#">em cartaz</a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
