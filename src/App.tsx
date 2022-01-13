e768ecda755d5d896a30ad2f53fd043ae1724601
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-01-13 17:21:31 -0300

    add h3 title

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import logo from './logo.svg'
 import './styles.css'
 import Header from './components/Header'
+import MoviePoster from './components/MoviesPoster'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <div className="App">
+        <Header />
+        <h2 style={{ color: '#373737', fontSize: '32px', marginTop: '100px' }}>EM CARTAZ</h2>
+        <MoviePoster />
+      </div>
+    </>
   )
 }
 
